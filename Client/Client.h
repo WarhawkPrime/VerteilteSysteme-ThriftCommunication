c672756b9928d5b69511d7164e53013213e43b8d
WarhawkPrime <dennis.wilpert@web.de>
2020-05-21 10:24:50 +0200

    angefangen mit client. In Zentrale kann gearbeitet werden

--- Client/Client.h
+++ Client/Client.h
@@ -1,5 +1,12 @@
 #pragma once
+
+#include "TCP_Socket.h"
 class Client
 {
+public:
+
+
+private:
+	TCP_Socket tcp;
 };
 
--- Zentrale/HTTP_Server.cpp
+++ Zentrale/HTTP_Server.cpp
@@ -1,1 +1,5 @@
 #include "HTTP_Server.h"
+
+HTTP_Server::HTTP_Server()
+{
+}
