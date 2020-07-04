01a5b57980834a6bc681605f5645826c1bff24e0
= <=>
2020-07-04 18:33:01 +0200

    tests

--- Test/main.cpp
+++ Test/main.cpp
@@ -55,156 +55,164 @@ void i() {
 }
 
 TEST_SUITE("Tests für Funktionale und Nichtfunktionale Anforderungen mit der Zentrale am laufen" * doctest::description("Test mit Zentrale")) 
-{
+	{
 	
 
-	TEST_CASE("Start der Zentrale")
-	{
-		i();
-
-		//Anforderungen, deren Umsetzung direkt der Zweckbestimmung des Produkts dienen
-		SUBCASE("Funktionale Tests")
+		TEST_CASE("Start der Zentrale")
 		{
-			/*
-			SUBCASE("Kommt die Nachricht an der Zentrale an") {
-				Sensor_Management sm;
-				sm.create_temperatureSensor(0, "8");
+			i();
 
-				CHECK(udp->get_statusVar() == 0);
-			}
-			*/
-
-			SUBCASE("HTTP REQUEST and RESPONSE") {
-				Client c;
-
-				std::string a = "=";
-				std::string sPara = "param1";
-				sPara += a + "0";
-
-				c.build_header("temp.txt", sPara);
-
-				//c.build_header("temp.txt", 0);
-				c.rec_message();
-				c.interprete_message();
-				CHECK(c.getResponse().message != "");
-			}
-
-			SUBCASE("Thrift Message") {
-
-				ThriftClient t;
-
-				CHECK(t.startThrift() == true);
-
-			}
-
-			SUBCASE("MQTT") {
-
-				//Sensor_Management sm;
-				//sm.create_windMQTTSensor(0, "8");
-				//sm.get_mqttSensor()->repeater(0,8,0);
-				MQTT_Sensor* ms = new MQTT_Sensor(0,"8");
-				
-				CHECK(ms->repeater(0,8,0, "windsensor");				
-			}
-
-
-
-
-		} 
-
-		//Anforderungen, die unspezifisch für das Programm sind und die Qualität der Funktionalen Anforderungen überprüft
-		SUBCASE("Nichtfunktionale Tests")
-		{
-			/*
-			SUBCASE("Paketverlust 1 %")
+			//Anforderungen, deren Umsetzung direkt der Zweckbestimmung des Produkts dienen
+			SUBCASE("Funktionale Tests")
 			{
-				int counter;
-				for (size_t i = 0; i < 250; i++)
-				{
+				/*
+				SUBCASE("Kommt die Nachricht an der Zentrale an") {
 					Sensor_Management sm;
-
 					sm.create_temperatureSensor(0, "8");
-					if (udp->get_statusVar() == 0) {
-						counter++;
-					}
-					sm.create_brightnessSensor(0, "8");
-					if (udp->get_statusVar() == 0) {
-						counter ++;
-					}
-					sm.create_humiditySensor(0, "8");
-					if (udp->get_statusVar() == 0) {
-						counter ++;
-					}
-					sm.create_windSensor(0, "8");
-					if (udp->get_statusVar() == 0) {
-						counter ++;
-					}
+
+					CHECK(udp->get_statusVar() == 0);
 				}
-				CHECK(counter >= (1000 - 10));
-			}
+				*/
 
-			SUBCASE("Stabiles gleichzeitiges Senden") {
-				//nichtfunktionale
-				//zeit, umgebungsbedingung, performance
-
-					Sensor_Management sm;
-					sm.create_temperatureSensor(0, "8");
-
+				SUBCASE("HTTP REQUEST and RESPONSE") {
 					Client c;
+
 					std::string a = "=";
 					std::string sPara = "param1";
 					sPara += a + "0";
 
 					c.build_header("temp.txt", sPara);
+
+					//c.build_header("temp.txt", 0);
 					c.rec_message();
 					c.interprete_message();
-
-
-					bool t1;
-					bool t2;
-					if (c.getResponse().message != "") {
-						t1 = true;
-					}
-					else {
-						t1 = false;
-					}
-
-					if (udp->get_statusVar() == 0) {
-						t2 = true;
-					}
-					else {
-						t2 = false;
-					}
-
-					CHECK(t1 == true);
-					CHECK(t2 == true);
-			*/
-			
-			
-			SUBCASE("thrift Dauer") {
-
-				ThriftClient t;
-				auto start = high_resolution_clock::now();
-
-				for (int i = 0; i < 10; i++ ) {
-					t.startThrift();
+					CHECK(c.getResponse().message != "");
 				}
 
-				auto stop = high_resolution_clock::now();
-				auto duration = duration_cast<microseconds>(stop - start);
+				SUBCASE("Thrift Message") {
 
-				CHECK(duration.count() < 1000000 );
+					ThriftClient t;
+
+					CHECK(t.startThrift() == true);
+
+				}
+
+				SUBCASE("MQTT") {
+
+					//Sensor_Management sm;
+					//sm.create_windMQTTSensor(0, "8");
+					//sm.get_mqttSensor()->repeater(0,8,0);
+					MQTT_Sensor* ms = new MQTT_Sensor(0,"8");
+					
+					CHECK(ms->repeater(0,8,0, "windsensor");				
+				}
+
+
+
+
+			} 
+
+			//Anforderungen, die unspezifisch für das Programm sind und die Qualität der Funktionalen Anforderungen überprüft
+			SUBCASE("Nichtfunktionale Tests")
+			{
+				/*
+				SUBCASE("Paketverlust 1 %")
+				{
+					int counter;
+					for (size_t i = 0; i < 250; i++)
+					{
+						Sensor_Management sm;
+
+						sm.create_temperatureSensor(0, "8");
+						if (udp->get_statusVar() == 0) {
+							counter++;
+						}
+						sm.create_brightnessSensor(0, "8");
+						if (udp->get_statusVar() == 0) {
+							counter ++;
+						}
+						sm.create_humiditySensor(0, "8");
+						if (udp->get_statusVar() == 0) {
+							counter ++;
+						}
+						sm.create_windSensor(0, "8");
+						if (udp->get_statusVar() == 0) {
+							counter ++;
+						}
+					}
+					CHECK(counter >= (1000 - 10));
+				}
+
+				SUBCASE("Stabiles gleichzeitiges Senden") {
+					//nichtfunktionale
+					//zeit, umgebungsbedingung, performance
+
+						Sensor_Management sm;
+						sm.create_temperatureSensor(0, "8");
+
+						Client c;
+						std::string a = "=";
+						std::string sPara = "param1";
+						sPara += a + "0";
+
+						c.build_header("temp.txt", sPara);
+						c.rec_message();
+						c.interprete_message();
+
+
+						bool t1;
+						bool t2;
+						if (c.getResponse().message != "") {
+							t1 = true;
+						}
+						else {
+							t1 = false;
+						}
+
+						if (udp->get_statusVar() == 0) {
+							t2 = true;
+						}
+						else {
+							t2 = false;
+						}
+
+						CHECK(t1 == true);
+						CHECK(t2 == true);
+				*/
+				
+				
+				SUBCASE("thrift Dauer") {
+
+					ThriftClient t;
+					auto start = high_resolution_clock::now();
+
+					for (int i = 0; i < 10; i++ ) {
+						t.startThrift();
+					}
+
+					auto stop = high_resolution_clock::now();
+					auto duration = duration_cast<microseconds>(stop - start);
+
+					CHECK(duration.count() < 1000000 );
+				}
+
+	
+				SUBCASE("MQTT Quality") {
+				
+					MQTT_Sensor* ms = new MQTT_Sensor(0,"8");
+									ms->repeater(0,8,0, "windsensor");	
+
+					bool t = true;
+					CHECK(t == true);
+				}
+		
 			}
-
-/*
-			SUBCASE("MQTT Quality") {
-			
-				MQTT_Sensor* ms = new MQTT_Sensor(0,"8");
-                                ms->repeater(0,8,0, "windsensor");	
-
-				bool t = true;
-				CHECK(t == true);
-			}
-*/	
-	}	
-}
+		}
+	}
+	
+	
+	
+	
+	
+	
+	
